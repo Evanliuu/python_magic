6bdaeb67763aa57bef83de2be3b7c3af07a4b059
Evanliuu <1872863416@qq.com>
2019-05-30 22:20:38 +0800

    update

--- trunk/testing/test1.py
+++ trunk/testing/test1.py
@@ -18,9 +18,11 @@ class Crawler(object):
         self.like_movie = like_movie
 
     def parameter(self):
+        android_ua = 'Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+        chrome_ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+
         headers = {
-            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-                          ' (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
+            "User-Agent": android_ua,
             'Host': 'www.623zz.com'
         }
         params = {
@@ -31,7 +33,6 @@ class Crawler(object):
     def get_web_page(self, url=None, purpose=GET, headers=None, params=None):
         url = url or self.source_url
         headers = headers or self.parameter()[0]
-        params = params or self.parameter()[1]
 
         try:
             if purpose == GET:
@@ -49,7 +50,7 @@ class Crawler(object):
             return None
 
     @staticmethod
-    def parse_url(html):
+    def search_total_movie(html):
         final_ufl = []
         soup = BeautifulSoup(html, 'lxml')
         lines = soup.find_all('a', class_='module_play_img')
@@ -62,15 +63,37 @@ class Crawler(object):
         else:
             return None
 
+    @staticmethod
+    def search_download_url(html):
+        soup = BeautifulSoup(html, 'lxml')
+        lines = soup.find('p', class_='player_list')
+        tag = lines.find('a')
+        url = tag['href']
+        return url
+
+    def login_movie_page(self):
+        params = self.parameter()[1]
+        resp = self.get_web_page(purpose=GET, params=params)
+        return resp
+
     def main(self):
-        html = self.get_web_page(purpose=GET)
-        if html:
-            total_url = self.parse_url(html.text)
+        # 登录到电影网站
+        resp = self.login_movie_page()
+        if resp:
+            # 获取指定电影类型的总数量
+            total_url = self.search_total_movie(resp.text)
             if total_url:
+                # 选择需要的电影名称
                 like_url = total_url[0]
-                print(like_url)
-                download_url = urljoin(self.base_url, like_url)
-                print(download_url)
+                movie_url = urljoin(self.base_url, like_url)
+                resp = self.get_web_page(url=movie_url, purpose=GET)
+                if resp:
+                    # 获取电影的最终链接
+                    download_url = self.search_download_url(resp.text)
+                    if download_url:
+                        download_url = urljoin(self.base_url, download_url)
+                        resp = self.get_web_page(url=download_url, purpose=GET)
+                        print(resp.text)
 
 
 if __name__ == '__main__':
