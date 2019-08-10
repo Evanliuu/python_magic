6c63b426d630a4b3de0231c51c80f7e2701608be
evaliu <1872863416@qq.com>
2019-08-10 14:02:26 +0800

    添加pxssh的使用例子

---
+++ magic_code/python_example/function_module_example/pxssh_sample.py
@@ -0,0 +1,28 @@
+from pexpect import pxssh
+import getpass
+
+
+def run():
+    try:
+        s = pxssh.pxssh()
+        hostname = input('hostname: ')
+        username = input('username: ')
+        password = input('password: ')
+        # password = getpass.getpass('password: ')  # pycharm中无法使用此功能
+
+        # 利用 login 方法进行 ssh 登录，初始 prompt 为'$' , '#'或'>'
+        s.login(hostname, username, password, original_prompt='[$#>]')
+        # 发送命令
+        s.sendline('ls -l')
+        # 匹配 prompt
+        s.prompt()
+        # 将 prompt 前所有内容打印出，即命令 ' ls -l ' 的执行结果.
+        print(s.before)
+        # 退出 ssh session
+        s.logout()
+    except pxssh.ExceptionPxssh as ex:
+        print('ssh error, error msg: {}'.format(ex))
+
+
+if __name__ == '__main__':
+    run()
