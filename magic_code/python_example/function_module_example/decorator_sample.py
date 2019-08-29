86364e2c6431c72144985d2acbea298e2020ab93
Evanliuu <1872863416@qq.com>
2019-08-29 21:17:54 +0800

    Optimize the decorator_sample.py program

--- magic_code/python_example/function_module_example/decorator_sample.py
+++ magic_code/python_example/function_module_example/decorator_sample.py
@@ -1,22 +1,38 @@
-def func(value=None):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            result = list()
-            # 添加调用函数的实参
-            result.append(*args, **kwargs)
-            # 添加调用函数的返回值
-            result.append(func(*args, **kwargs))
-            # 添加装饰器的实参
-            result.append(value)
+# 二层装饰器
+def decorator(func):  # 执行函数
+    def wrapper(*args, **kwargs):  # 函数的实参
+        result = dict()
+        result['from_func_parameter'] = [args or kwargs]
+        result['from_func_return_value'] = func(*args, **kwargs)
+        return result
+    return wrapper
+
+
+# 三层装饰器
+def three_tool(value=None):  # 装饰器的实参
+    def decorator(func):  # 执行函数
+        def wrapper(*args, **kwargs):  # 函数的实参
+            result = dict()
+            result['from_func_parameter'] = [args or kwargs]
+            result['from_func_return_value'] = func(*args, **kwargs)
+            result['from_decorator_parameter'] = value
             return result
         return wrapper
     return decorator
 
 
-@func('Hello World!')
-def function(*args):
+@decorator
+def example1(*args):
+    return 'hi {}'.format(args)
+
+
+@three_tool('Hello World!')
+def example2(*args):
     return 'hi {}'.format(args)
 
 
 if __name__ == '__main__':
-    print(function('Evan'))
+    print('二层装饰器结果：')
+    print(example1('Evan1'))
+    print('三层装饰器结果：')
+    print(example2('Evan2'))
