dea40a95b8dd3eaac8c1945c3e8ca56fd4a32b3e
evaliu <1872863416@qq.com>
2019-08-29 18:54:22 +0800

    添加signal模块使用例子

---
+++ magic_code/python_example/function_module_example/signal_sample.py
@@ -0,0 +1,17 @@
+import signal
+import time
+import datetime
+
+
+def my_handle(*args):
+    print('time out!')
+
+
+# signal.SIGALRM 只能在Linux端使用
+signal.signal(signal.SIGALRM, my_handle)
+# 设置监听时间，如果程序运行时间超过2秒还没结束就会执行 "my_handle" 函数
+signal.alarm(2)
+
+# Running
+print(datetime.datetime.now())
+time.sleep(5)
