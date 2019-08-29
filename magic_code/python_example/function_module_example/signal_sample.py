7c206b9423551074a69786ebb6da134b68232458
Evanliuu <1872863416@qq.com>
2019-08-29 21:28:05 +0800

    Optimize the signal_sample.py program

--- magic_code/python_example/function_module_example/signal_sample.py
+++ magic_code/python_example/function_module_example/signal_sample.py
@@ -3,15 +3,29 @@ import time
 import datetime
 
 
-def my_handle(*args):
-    print('time out!')
+def signal_decorator(func):
+    def wrapper(*args, **kwargs):
+        def my_handle(*args):
+            print('time out!')
+            exit()
+
+        # TODO signal.SIGALRM 只能在Linux端使用
+        signal.signal(signal.SIGALRM, my_handle)
+        # 设置监听时间，如果func运行时间超过2秒还没结束就会强制中断，并执行 "my_handle" 函数
+        signal.alarm(2)
+
+        try:
+            func(*args, **kwargs)
+        finally:
+            signal.alarm(0)  # Disable signal function
+    return wrapper
 
 
-# TODO signal.SIGALRM 只能在Linux端使用
-signal.signal(signal.SIGALRM, my_handle)
-# 设置监听时间，如果程序运行时间超过2秒还没结束就会执行 "my_handle" 函数
-signal.alarm(2)
+@signal_decorator
+def main():
+    print(datetime.datetime.now())
+    time.sleep(5)
 
-# Running
-print(datetime.datetime.now())
-time.sleep(5)
+
+if __name__ == '__main__':
+    main()
