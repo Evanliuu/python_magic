bf10cc5a846521806a8b5aaba272af30ba9865de
evaliu <1872863416@qq.com>
2019-11-10 14:54:08 +0800

    update comment

--- trunk/python_scripts/projects/automated_testing/apollo_automation.py
+++ trunk/python_scripts/projects/automated_testing/apollo_automation.py
@@ -2,7 +2,7 @@
 The file is located on a Windows central control machine at BPM2 automation station in Barbados
 This program will start three multi-threads:
 1. Start an rpc-socket service that listens for all incoming data from the Apollo server
-2. Read the local access data table to fetch each row of data
+2. Open the local access data table to read each row of data and transfer it to the corresponding Apollo server
 3. Update the received Apollo server data into the access data table
 python version: 3.7
 """
