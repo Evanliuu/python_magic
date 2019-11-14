999270fca97e27191503e46ba1da739cb6bd631a
Evanliuu <1872863416@qq.com>
2019-11-14 21:41:12 +0800

    优化功能函数

--- trunk/python_scripts/python_examples/crawler_example/beautifulsoup_sample.py
+++ trunk/python_scripts/python_examples/crawler_example/beautifulsoup_sample.py
@@ -17,13 +17,36 @@ and they lived at the bottom of a well.</p>
 
 
 def parse():
+    # 初始化对象
     soup = BeautifulSoup(html_doc, 'lxml')
-    # soup.prettify()  # 补全HTML代码
-    # print(soup.p)   # 获取标签信息
-    # print(soup.p.name)  # 获取标签名称
-    # print(soup.p.string)  # 获取标签文本
-    # print(soup.text)  # 获取所有文本信息
-    # print(soup.p['class'])  # 获取标签内class属性值
+    # soup = BeautifulSoup(open('example.html'), 'lxml')
+
+    # 补全HTML代码
+    soup.prettify()
+    # 获取标签名称
+    print(soup.p)  # 获取整条p标签
+    print(soup.p.name)  # 获取p标签名称
+    # 获取标签属性
+    print(soup.p.attrs)  # 获取p标签内的所有属性
+    print(soup.p['class'])  # 获取p标签内的class属性值
+    # 获取标签文本
+    print(soup.p.string)  # 获取p标签的文本信息，如果p标签内包含了多个子节点并有多个文本时返回None
+    print(soup.p.strings)  # 获取p标签内的所有文本信息，返回一个生成器
+    print(soup.p.text)  # 获取p标签内的所有文本信息，返回一个列表
+    print(soup.stripped_strings)  # 去掉空白，保留所有的文本，返回一个生成器
+
+    # 获取父子节点
+    print(soup.p.parent)  # 获取p标签的父节点
+    print(soup.p.parents)  # 获取p标签的所有父节点，返回一个生成器
+    print(soup.p.contents)  # 获取p标签内的所有子节点，返回一个列表
+    print(soup.p.children)  # 获取p标签内的所有子节点，返回一个生成器
+    # 获取子孙节点
+    print(soup.p.descendants)  # 获取p标签内的所有子孙节点，返回一个生成器
+    # 获取兄弟节点
+    print(soup.a.previous_sibling)  # 获取a标签的上一个兄弟节点
+    print(soup.a.previous_siblings)  # 获取a标签的上一个兄弟节点，返回一个生成器
+    print(soup.a.next_sibling)  # 获取a标签的下一个兄弟节点
+    print(soup.a.next_siblings)  # 获取a标签的下一个兄弟节点，返回一个生成器
 
     # 使用re.compile('Lacie')为in的匹配关系，不使用Regex则是完全匹配，如：text='Lacie'
     print(soup.find_all('a', text=re.compile('Lacie'), limit=1))  # 根据文本定位 (limit: 限制匹配个数)
