8da6750c0d74fec29f5cedf4d9f3f7fa367e7a7f
evaliu <1872863416@qq.com>
2019-12-19 16:31:30 +0800

    update

--- trunk/unit_testing/test1.py
+++ trunk/unit_testing/test1.py
@@ -2,22 +2,23 @@
 import requests
 
 
-def request_url(url):
-    response = requests.get(url='https://www.baidu.com/s?ie=UTF-8&wd=python')  # 用GET请求访问百度首页
-    print(response.text)  # 打印百度首页的HTML文本
+# r = requests.get(url='http://httpbin.org/get')  # 使用GET请求访问
+# print(r.text)  # 打印网页的HTML文本
 
-    # 请求头参数
-    headers = {
-        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/78.0.3904.108 Safari/537.36'
-    }
-    params = {
-        'ie': 'UTF-8',
-        'wd': 'python'
-    }
-    response = requests.get(url=url, headers=headers, params=params)  # Get请求
-    print(response.text)
+data = {
+    'name': 'Evan',
+    'age': '24'
+}
 
+# 请求头参数
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/78.0.3904.108 Safari/537.36'
+}
 
-if __name__ == '__main__':
-    request_url(url='https://www.baidu.com/')
+# 第一次访问，并设置cookies
+r1 = requests.get(url='http://httpbin.org/cookies/set/number/123456789')
+print('r1: {}'.format(r1.text))
+# 第二次访问
+r2 = requests.get(url='http://httpbin.org/cookies')
+print('r2: {}'.format(r2.text))
