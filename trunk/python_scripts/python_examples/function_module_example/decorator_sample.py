7ed8c43d80d027074de993f6ffa24a8221cc5c19
evaliu <1872863416@qq.com>
2020-01-15 15:04:07 +0800

    optimize decorator program

--- trunk/python_scripts/python_examples/function_module_example/decorator_sample.py
+++ trunk/python_scripts/python_examples/function_module_example/decorator_sample.py
@@ -9,7 +9,7 @@ def decorator(func):  # 执行函数
 
 
 # 三层装饰器
-def three_tool(value=None):  # 装饰器的实参
+def full_decorator(value=None):  # 装饰器的实参
     def decorator(func):  # 执行函数
         def wrapper(*args, **kwargs):  # 函数的实参
             result = dict()
@@ -26,13 +26,13 @@ def example1(*args):
     return 'hi {}'.format(args)
 
 
-@three_tool('Hello World!')
+@full_decorator('Hello World!')
 def example2(*args):
     return 'hi {}'.format(args)
 
 
 if __name__ == '__main__':
     print('二层装饰器结果：')
-    print(example1('This is example1'))
+    print(example1('example1'))
     print('三层装饰器结果：')
-    print(example2('This is example2'))
+    print(example2('example2'))
