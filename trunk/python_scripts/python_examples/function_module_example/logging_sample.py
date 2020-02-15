8c9b932649e40be3a7b54ecde9e06ec939f89853
Evanliuu <1872863416@qq.com>
2020-02-15 15:57:32 +0800

    优化logger例子

--- trunk/python_scripts/python_examples/function_module_example/logging_sample.py
+++ trunk/python_scripts/python_examples/function_module_example/logging_sample.py
@@ -1,9 +1,9 @@
-"""日志一共分为5个级别，从低到高分别是: DEBUG、INFO、WARNING、ERROR、CRITICAL (所有的设定默认级别都是 WARNING)
-1. DEBUG:        详细的信息，通常只出现在诊断问题上；
-2. INFO:         确认一切按预期运行；
-3. WARNING:      一个迹象表明，一些意想不到的事情发生了，或表明一些问题在不久的将来会发生(例如：磁盘空间低...等等)
-4. ERROR:        比较严重的问题，软件或程序没能执行一些功能；
-5. CRITICAL:     十分严重的错误，这表明程序本身可能无法继续运行。
+"""日志一共分为5个级别，从低到高分别是: DEBUG、INFO、WARNING、ERROR、CRITICAL (所有的默认级别都是 WARNING)
+1. DEBUG:           # 程序调试信息，通常只会出现在诊断问题上
+2. INFO:            # 程序运行正常，增加一些提示信息
+3. WARNING:         # 程序警告信息，用于一些突发事件
+4. ERROR:           # 比较严重的问题，程序没能执行一些功能
+5. CRITICAL:        # 十分严重的错误，程序可能会中断或者崩溃
 
 <<日志格式说明>>
 %(levelno)s         # 打印日志级别的数值
@@ -28,31 +28,32 @@ import logging
 __author__ = 'Evan'
 
 
-def logger_sample(format_info='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                  level_info=logging.WARNING, record_file_path='', record_file_mode=''):
+def logger_sample(save_log_path='', file_mode='a',
+                  format_info='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                  level_info=logging.INFO):
     """
     打印日志消息到控制台，或保存日志消息到文件中
+    :param save_log_path: 日志文件保存路径，默认为空，如果传入有效的文件路径，控制台则不打印日志消息，仅保存日志消息到文件中
+    :param file_mode: 文档打开模式，默认为追加模式
     :param format_info: 日志格式，默认是输出日志的打印时间，模块名，输出的日志级别，以及输入的日志内容。
-    :param level_info: 日志级别，默认是 WARNING，只有大于等于该等级的消息才会被记录
-    :param record_file_path: 日志文件保存的路径，默认为空，如果传合法的文件路径进来，控制台则不打印日志消息，仅保存日志消息到文件中
-    :param record_file_mode: 打开文档的模式
+    :param level_info: 日志级别，默认是 INFO，只有大于等于该等级的消息才会被记录
     :return:
     """
-    logging.basicConfig(level=level_info, format=format_info, filename=record_file_path, filemode=record_file_mode)
-    # Use logging
-    logging.debug('This is a loggging debug message')
-    logging.info('This is a loggging info message')
-    logging.warning('This is loggging a warning message')
-    logging.error('This is an loggging error message')
-    logging.critical('This is a loggging critical message')
+    logging.basicConfig(filename=save_log_path, filemode=file_mode, level=level_info, format=format_info)
+    # logging test
+    logging.debug('This is debug message')
+    logging.info('This is info message')
+    logging.warning('This is warning message')
+    logging.error('This is error message')
+    logging.critical('This is critical message')
 
 
-def logger_full(record_file_path, record_file_mode,
+def logger_full(save_log_path, file_mode='a',
                 format_info='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
     """
     打印日志消息到控制台，并保存日志消息到文件中
-    :param record_file_path: 日志文件保存的路径
-    :param record_file_mode: 打开文档的模式
+    :param save_log_path: 日志文件保存路径
+    :param file_mode: 文档打开模式，默认为追加模式
     :param format_info: 日志格式，默认是输出日志的打印时间，模块名，输出的日志级别，以及输入的日志内容。
     :return:
     """
@@ -61,7 +62,7 @@ def logger_full(record_file_path, record_file_mode,
     logger.setLevel(logging.DEBUG)  # Log等级总开关，（设置级别后，不管是输出到文件还是控制台都要大于等于该级别才会被记录）
 
     # 第二步，创建一个handler，用于写入日志文件
-    fh = logging.FileHandler(record_file_path, mode=record_file_mode)
+    fh = logging.FileHandler(save_log_path, mode=file_mode)
     fh.setLevel(logging.DEBUG)  # 输出到file的log等级的开关
     fh.setFormatter(logging.Formatter(format_info))  # 定义handler的输出格式
     logger.addHandler(fh)  # 将logger添加到handler里面
@@ -72,12 +73,12 @@ def logger_full(record_file_path, record_file_mode,
     ch.setFormatter(logging.Formatter(format_info))  # 定义handler的输出格式
     logger.addHandler(ch)  # 将logger添加到handler里面
 
-    # Use logging
-    logger.debug('This is a logger debug message')
-    logger.info('This is a logger info message')
-    logger.warning('This is a logger warning message')
-    logger.error('This is a logger error message')
-    logger.critical('This is a logger critical message')
+    # logger test
+    logger.debug('This is debug message')
+    logger.info('This is info message')
+    logger.warning('This is warning message')
+    logger.error('This is error message')
+    logger.critical('This is critical message')
 
     # logging异常处理
     try:
@@ -87,9 +88,10 @@ def logger_full(record_file_path, record_file_mode,
 
 
 if __name__ == '__main__':
-    # 仅打印日志消息到控制台，不保存
-    logger_sample()
+    # TODO 仅打印日志消息到控制台，不保存
+    # logger_sample()
+
     # 在当前路径下创建log_sample.txt，保存日志消息到其中，不打印消息到控制台
-    logger_sample(record_file_path='./log_sample.txt', record_file_mode='w')
-    # 在当前路径下创建log_sample.txt，保存日志消息到其中，并打印消息到控制台
-    logger_full(record_file_path='./log_sample.txt', record_file_mode='w')
+    logger_sample(save_log_path='./log_sample.txt', file_mode='w')
+    # 在当前路径下创建log_full.txt，保存日志消息到其中，并打印消息到控制台
+    logger_full(save_log_path='./log_full.txt', file_mode='w')
