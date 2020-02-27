8675e259df1ff425d1b1a3fa474a5a089ab715b5
Evanliuu <1872863416@qq.com>
2020-02-27 21:39:14 +0800

    优化程序

--- trunk/python_scripts/data_visualization/plot_example/histogram_example.py
+++ trunk/python_scripts/data_visualization/plot_example/histogram_example.py
@@ -1,24 +1,26 @@
 # -*- coding:utf-8 -*-
+import numpy
 from matplotlib import pyplot as plt
 
 __author__ = 'Evan'
 
 
-def draw_histogram(title, data_list=[], x_label=(), y_label=()):
+def draw_histogram():
     """
     绘制柱状图
-    :param str title: 图片标题
-    :param list data_list: 数据列表
-    :param tuple x_label: （X轴标签，X轴刻度标签）
-    :param tuple y_label:（Y轴标签，Y轴刻度标签）
     :return:
     """
     # 处理中文乱码
     plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
     plt.rcParams['axes.unicode_minus'] = False
 
+    plt.title('四个直辖市GDP大比拼')  # 添加标题
+
+    data_list = [[100, 300, 500, 700], [200, 400, 600, 800]]  # 柱状图数据
     item_range = range(len(data_list[0]))  # 计算所有数据的长度
-    plt.title(title)  # 添加标题
+
+    x_label = ['城市分布', ['北京市', '上海市', '天津市', '重庆市']]  # X轴大标签和刻度标签
+    y_label = ['GDP', [50, 1000]]  # Y轴大标签和刻度标签
 
     # 绘X轴刻度的第一个柱形图（宽度0.4）
     plt.bar(item_range, data_list[0], align='center',
@@ -44,9 +46,34 @@ def draw_histogram(title, data_list=[], x_label=(), y_label=()):
     # plt.savefig('./example.jpg')  # 保存图片
 
 
+def draw_hist():
+    """
+    绘制直方图
+    :return:
+    """
+    # 处理中文乱码
+    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.rcParams['axes.unicode_minus'] = False
+
+    # 随机生成服从正态分布的数据
+    data = numpy.random.randn(10000)
+
+    """
+    hist部分参数用法:
+    data：必选参数，绘图数据
+    bins：直方图的长条形数目，可选项，默认为10
+    normed：是否将得到的直方图向量归一化，可选项，默认为0，代表不归一化，显示频数。normed=1，表示归一化，显示频率。
+    facecolor：长条形的颜色
+    edgecolor：长条形边框的颜色
+    alpha：透明度
+    """
+    plt.hist(data, bins=40, density=0, facecolor="blue", edgecolor="black", alpha=0.7)
+    plt.xlabel("区间")  # 显示横轴标签
+    plt.ylabel("频数/频率")  # 显示纵轴标签
+    plt.title("频数/频率分布直方图")  # 显示图标题
+    plt.show()
+
+
 if __name__ == '__main__':
-    city_title = '四个直辖市GDP大比拼'
-    data = [[100, 300, 500, 700], [200, 400, 600, 800]]
-    x_label_tuple = ('城市分布', ['北京市', '上海市', '天津市', '重庆市'])
-    y_label_tuple = ('GDP', [50, 1000])
-    draw_histogram(title=city_title, data_list=data, x_label=x_label_tuple, y_label=y_label_tuple)
+    draw_histogram()  # 绘制柱形图
+    draw_hist()  # 绘制直方图
