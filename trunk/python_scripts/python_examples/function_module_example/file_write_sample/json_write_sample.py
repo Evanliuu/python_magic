01d226d9f99ed445b9f7c521114fa489c1549706
evaliu <1872863416@qq.com>
2020-03-05 17:41:01 +0800

    优化json函数

--- trunk/python_scripts/python_examples/function_module_example/file_write_sample/json_write_sample.py
+++ trunk/python_scripts/python_examples/function_module_example/file_write_sample/json_write_sample.py
@@ -12,7 +12,7 @@ def write_json_data(write_info, file_name='json_file'):
     :return:
     """
     with open('{}.json'.format(file_name), 'w', encoding='utf-8') as wf:
-        # 将文本字符串转换为JSON对象，如果有中文ensure_ascii要设置为False，indent代表缩进字符个数
+        # 将Python对象序列化为JSON中的字符串对象，如果有中文ensure_ascii要设置为False，indent代表缩进字符个数
         wf.write(json.dumps(write_info, ensure_ascii=False, indent=2) + '\n')
 
 
@@ -22,8 +22,9 @@ def read_json_data(file_name='json_file'):
     :param file_name: json文件名称
     :return:
     """
+    print(json.load(open('file_name.json')))  # 使用load方法接受一个文件句柄
     with open('{}.json'.format(file_name), 'r', encoding='utf-8') as rf:
-        # 将JSON对象转换为字典
+        # 将JSON中的字符串对象反序列化为Python对象
         json_dict = json.loads(rf.read())
         return json_dict
 
