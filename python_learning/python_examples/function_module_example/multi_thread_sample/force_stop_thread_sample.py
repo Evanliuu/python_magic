90b5e5e990f7b95a71bd9b176cc8cd370dc9ded6
evaliu <1872863416@qq.com>
2020-04-11 15:50:17 +0800

    Optimize force stop thread function

--- python_learning/python_examples/function_module_example/multi_thread_sample/force_stop_thread_sample.py
+++ python_learning/python_examples/function_module_example/multi_thread_sample/force_stop_thread_sample.py
@@ -1,7 +1,7 @@
 """
 强制结束线程方法：
 1> 使用stop_thread函数关闭指定的单个线程
-2> 设置守护进程，守护进程结束后会关闭所有的子线程 --> Set daemon=True
+2> 设置当前函数为守护进程，函数结束后会关闭所有的子线程 --> Set daemon=True
 """
 # -*- coding:utf-8 -*-
 import threading
@@ -38,34 +38,39 @@ def use_stop_thread_function():
 
 class UseDaemonFunction(object):
 
-    def __init__(self):
-        self.stop_flag = False
-
     def test_2(self, times):
         time.sleep(times)
         print('times: {}'.format(times))
-        if times == 3:  # If times equals 3, exit the daemon
-            self.stop_flag = True
 
     def main(self):
+        # 设置主线程
+        self.threads = threading.Thread(target=self._main, args=())
+        self.threads.start()
+
+    def _main(self):
         print('Use daemon function')
         threads = []
-        for i in range(10):
+        for i in range(20):
+            # 开启20个子线程
             t = threading.Thread(target=self.test_2, args=(i, ))
-            t.daemon = True
+            t.daemon = True  # 设置当前函数为守护进程
             threads.append(t)
 
         for i in threads:
             i.start()
 
-        while True:
-            if self.stop_flag:
-                break
+        for i in threads:
+            i.join()
         print('Threads have been stopped')
 
 
 if __name__ == "__main__":
-    use_stop_thread_function()
+    # use_stop_thread_function()
     # ————分割线————
     use_daemon_funcion = UseDaemonFunction()
-    use_daemon_funcion.main()
+    use_daemon_funcion.main()  # 执行20个子线程
+
+    time.sleep(5)
+    # 杀死主线程，所有的子线程都会被关闭
+    stop_thread(tid=use_daemon_funcion.threads)
+    print('All done')
