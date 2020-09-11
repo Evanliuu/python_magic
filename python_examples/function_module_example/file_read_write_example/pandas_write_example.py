9496ccda2966ab7b943ce96a0f010af2d0d5a215
evaliu <1872863416@qq.com>
2020-09-11 18:19:39 +0800

    update

---
+++ python_examples/function_module_example/file_read_write_example/pandas_write_example.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def write_excel(file_name, sheets, data):
+    """
+    使用pandas，写入多页工作表到Excel
+    :param file_name: 需要生成的Excel表格名
+    :param sheets: 需要写入的工作表名
+    :param data: 需要写入的数据
+    :return:
+    """
+    writer = pd.ExcelWriter(file_name)
+    for sheet in sheets:
+        df = pd.DataFrame(data)
+        df.to_excel(writer, sheet_name=sheet, index=False)
+    writer.save()
+    writer.close()
+
+
+if __name__ == '__main__':
+    write_excel(file_name='example.xlsx', sheets=['sheet1', 'sheet2'], data=[1, 2, 3])
