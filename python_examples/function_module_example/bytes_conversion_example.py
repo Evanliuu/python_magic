4bf689d1ffbfa3ac882e93254da695c4d0eebf86
evaliu <1872863416@qq.com>
2020-11-07 10:40:34 +0800

    update

--- python_examples/function_module_example/bytes_conversion_example.py
+++ python_examples/function_module_example/bytes_conversion_example.py
@@ -3,19 +3,18 @@ import os
 
 def bytes_conversion(size):
     """
-    字节转换
+    字节符号转换
     :param size: 字节大小（B）
     :return:
     """
     symbols = ('KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
     prefix = dict()
-    for i, s in enumerate(symbols, 1):
+    for i, s in enumerate(symbols, 1):  # 获取每个符号的字节数
         prefix[s] = 1 << i*10
-    for s in reversed(symbols):
+    for s in reversed(symbols):  # 判断文件大小所属的符号范围
         if int(size) >= prefix[s]:
-            value = float(size) / prefix[s]
-            return '%.2f%s' % (value, s)
-    return "%sB" % size
+            return '{:.2f} {}'.format(float(size) / prefix[s], s)
+    return "{} B".format(size)
 
 
 print(bytes_conversion(size=os.path.getsize(r'C:\Users\evaliu\Desktop\GifCam.exe')))
