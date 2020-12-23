5778cd76b592c26a976cd666a9de9e095368937d
evaliu <1872863416@qq.com>
2020-12-23 15:09:55 +0800

    update

--- python_examples/function_module_example/argparse_example.py
+++ python_examples/function_module_example/argparse_example.py
@@ -10,6 +10,6 @@ print(f'所有的输入参数：{sys.argv}')
 parser = argparse.ArgumentParser()
 parser.add_argument("-ip", "--ip", help="请填入其他主机的IP，例如：--ip 192.168.xx.xx,192.168.xx.xx")
 
-# 使用parse_args获取所有的输入参数
+# 使用parse_args获取输入值
 args = parser.parse_args()
-print(f'所有的输入参数：{sys.argv}')
+print(f'获取ip值：{args.ip}')
