eebe4e18fb9002f605e15e9f54899554a1b29c78
evaliu <1872863416@qq.com>
2021-01-06 10:11:01 +0800

    update

--- python_examples/function_module_example/datetime_example.py
+++ python_examples/function_module_example/datetime_example.py
@@ -5,13 +5,22 @@ print('当前时间：{}'.format(current_time))
 print('当前时间减8小时：{}'.format(current_time - datetime.timedelta(hours=8)))
 print('当前时间减1天：{}\n'.format(current_time - datetime.timedelta(days=1)))
 
-# 北京时间为东八区时间(UTC+8)，美国为西八区时间(UTC-8)，UTC0为世界标准时间
-print('北京时间转换为UTC0时间：{}'.format(current_time.astimezone(datetime.timezone(datetime.timedelta(hours=0)))))
-print('北京时间转换为西八区时间：{}\n'.format(current_time.astimezone(datetime.timezone(datetime.timedelta(hours=-8)))))
 
-print('字符转换为datetime类型：{}'.format(datetime.datetime.strptime('2020-12-01 08:00:00', '%Y-%m-%d %H:%M:%S')))
-print('datetime类型转换为字符：{}'.format(current_time.strftime('%Y-%m-%d %H:%M:%S')))
-
+# 时间计算
 now_time = datetime.datetime.now() - datetime.timedelta(days=1)
 print('两个时间差（秒）：{}'.format((now_time - current_time).total_seconds()))
 print('两个时间差（天）：{}'.format((now_time - current_time).days))
+
+
+# 时间转化
+print('取起始整点：{}'.format(current_time.replace(minute=0, second=0, microsecond=0)))
+
+
+# 时区转化：北京时间为东八区时间(UTC+8)，美国为西八区时间(UTC-8)，UTC0为世界标准时间
+print('北京时间转换为UTC0时间：{}'.format(current_time.astimezone(datetime.timezone(datetime.timedelta(hours=0)))))
+print('北京时间转换为西八区时间：{}\n'.format(current_time.astimezone(datetime.timezone(datetime.timedelta(hours=-8)))))
+
+
+# 类型转化
+print('字符转换为datetime类型：{}'.format(datetime.datetime.strptime('2020-12-01 08:00:00', '%Y-%m-%d %H:%M:%S')))
+print('datetime类型转换为字符：{}'.format(current_time.strftime('%Y-%m-%d %H:%M:%S')))
