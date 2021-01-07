140e2ccd8857e2b902008198bfa4c04242cd2b55
evaliu <1872863416@qq.com>
2021-01-07 16:54:13 +0800

    update

--- python_examples/function_module_example/argparse_example.py
+++ python_examples/function_module_example/argparse_example.py
@@ -3,12 +3,13 @@ import argparse
 
 
 # 使用sys获取所有的输入参数
-print(f'所有的输入参数：{sys.argv}')
+print(f'当前文件路径: {sys.argv[0]}')
+print(f'所有的输入参数：{sys.argv[1:]}')
 
 
 # 定义命令行参数
 parser = argparse.ArgumentParser()
-parser.add_argument("-ip", "--ip", help="请填入其他主机的IP，例如：--ip 192.168.xx.xx,192.168.xx.xx")
+parser.add_argument("-ip", "--ip", help="请填入主机IP，例如：-ip 192.168.xx.xx")
 
 # 使用parse_args获取输入值
 args = parser.parse_args()
