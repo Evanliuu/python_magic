ef749043a4b7ec0df09e6e0d47aeccb096a408de
evaliu <1872863416@qq.com>
2021-01-09 09:39:43 +0800

    优化manipulate_data_table方法

--- python_examples/database_example/mysql_db_example/use_mysqldb_example.py
+++ python_examples/database_example/mysql_db_example/use_mysqldb_example.py
@@ -46,7 +46,7 @@ class MysqlHandle(object):
             cursor = conn.cursor(MySQLdb.cursors.DictCursor) if as_dict else conn.cursor()
             cursor.execute(sql)
             if 'SELECT' in sql.upper():
-                return list(cursor.fetchall())
+                return cursor.fetchall()
             else:
                 conn.commit()
                 return True
@@ -62,11 +62,11 @@ class MysqlHandle(object):
             if conn:
                 conn.close()
 
-    def manipulate_data_table(self, data_table='', action='QUERY', **params):
+    def manipulate_data_table(self, data_table, action='QUERY', **params):
         """
         对数据表进行增删改查，通用方法
         :param data_table: 数据表名
-        :param action: 要执行的动作，'ADD' or 'UPDATE' or 'DELETE' or 'QUERY'
+        :param action: 要执行的事务，'ADD' or 'UPDATE' or 'DELETE' or 'QUERY'
         :param params: 需要使用的参数，dict
 
         params example:
@@ -92,41 +92,37 @@ class MysqlHandle(object):
                 }
         :return:
         """
+        assert data_table, '缺少data_table参数'
         if action == 'ADD':
             assert params.get('fields'), '缺少fields参数'
             assert params.get('values'), '缺少values参数'
             assert isinstance(params['fields'], list), 'fields必须是个列表'
             assert isinstance(params['values'], list), 'values必须是个列表'
             assert len(params['values']) == len(params['fields']), 'values长度必须和fields长度一致'
-
             # 拼接所有参数
             sql = """INSERT INTO {0}({1}) VALUES('{2}')""".format(data_table,
                                                                   ','.join(str(f) for f in params['fields']),
                                                                   '","'.join(str(v) for v in params['values']))
-            self.execute_sql_command(sql)
 
         elif action == 'UPDATE':
             assert params.get('field'), '缺少fields参数'
             assert params.get('new_value'), '缺少new_value参数'
-
             if params.get('where'):  # 处理有WHERE的情况
                 sql = """UPDATE {0} SET {1}='{2}' WHERE {3}""".format(data_table, params['field'],
                                                                       params['new_value'], params['where'])
             else:
                 sql = """UPDATE {0} SET {1}='{2}'""".format(data_table, params['field'], params['new_value'])
-            self.execute_sql_command(sql)
 
         elif action == 'DELETE':
             assert params.get('where'), '缺少where参数'
             sql = """DELETE FROM {0} WHERE {1}""".format(data_table, params['where'])
-            self.execute_sql_command(sql)
 
         else:  # Default QUERY
-            if params.get('fields'):  # 如果fields有值，返回指定的fields
+            if params.get('fields'):  # 如果fields有值，返回指定的field values
                 assert isinstance(params['fields'], list), 'fields必须是个列表'
                 fields = ','.join(str(f) for f in params['fields'])
 
-                if params.get('where'):
+                if params.get('where'):  # 处理有WHERE的情况
                     sql = """SELECT {0} FROM {1} WHERE {2}""".format(fields, data_table, params['where'])
                 else:
                     sql = """SELECT {0} FROM {1}""".format(fields, data_table)
@@ -136,11 +132,7 @@ class MysqlHandle(object):
                 else:
                     sql = """SELECT * FROM {}""".format(data_table)
 
-            data = self.execute_sql_command(sql)
-            if data:
-                return data
-            else:
-                return []
+        return self.execute_sql_command(sql)
 
 
 SQL = MysqlHandle()
