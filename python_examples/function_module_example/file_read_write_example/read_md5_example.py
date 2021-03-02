ccac76709ae9912be6f1c41989cf7928af03c29a
evaliu <1872863416@qq.com>
2021-03-02 18:14:18 +0800

    update

--- python_examples/function_module_example/file_read_write_example/read_md5_example.py
+++ python_examples/function_module_example/file_read_write_example/read_md5_example.py
@@ -4,15 +4,14 @@ import hashlib
 __author__ = 'Evan'
 
 
-def get_file_md5(path):
+def get_file_md5(filepath):
     """
     获取文件内容的MD5值
-    :param path: 文件所在路径
+    :param filepath: 文件所在路径
     :return:
     """
-    with open(path, 'rb') as file:
+    with open(filepath, 'rb') as file:
         data = file.read()
-
     diff_check = hashlib.md5()
     diff_check.update(data)
     md5_code = diff_check.hexdigest()
@@ -20,5 +19,5 @@ def get_file_md5(path):
 
 
 if __name__ == '__main__':
-    md5_str = get_file_md5(path=r'C:\Users\evaliu\Desktop\sample.txt')
+    md5_str = get_file_md5(filepath=r'C:\Users\evaliu\Desktop\sample.txt')
     print(md5_str)
